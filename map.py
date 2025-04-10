ed7d23c6c9038799dc6aaf8e64ed5290b8d82d2b
Lunarmagpie <Bambolambo0@gmail.com>
2025-04-10 15:23:57 -0400

    chnages

--- map.py
+++ map.py
@@ -81,7 +81,6 @@ def main():
 	c.set_edges([Angle(90, ac), Angle(90, cd), Angle(90), Angle(90)])
 	d.set_edges([Angle(90, bd), Angle(90, cd), Angle(90), Angle(90)])
 
-
 	pprint(phantom_fold(a, [a, b, c, d], {}))
 
 
